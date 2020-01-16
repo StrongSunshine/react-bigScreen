9df29ae6cf13a6ae8486140140935352b12513de
StrongSunshine <369532314@qq.com>
2020-01-16 19:00:24 +0800

    header修改

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,14 +3,16 @@ import ReactParticleLine from 'react-particle-line'
 
 import Header from '@/pages/header'
 
-import style from './index.scss'
+import styles from './index.scss'
 
 export default class Home extends PureComponent {
   render() {
     return (
       <ReactParticleLine>
-        <div className={style.homeBox}>
-          <Header />
+        <div className={styles.homeBox}>
+          <div className={styles.header}>
+            <Header />
+          </div>
         </div>
       </ReactParticleLine>
     )
